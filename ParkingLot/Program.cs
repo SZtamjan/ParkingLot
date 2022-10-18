76d5d5ed0bd33379307275b59ba533ce7976a84d
SZtamjan <tamjan001@gmail.com>
2022-10-18 23:57:38 +0200

    Initial commit

---
+++ ParkingLot/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            parking();
+        }
+        static void parking()
+        {
+            //Górna granica L1
+            Console.WriteLine("------------");
+            //Miejsca parkingowe górne L2
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            //Granica drogi
+            Console.WriteLine("|");
+            //Droga górna L3
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");//10 jest tego
+            //Granica drogi prawa
+            Console.WriteLine("|");
+            //Miejsca parkingowe środek góra L4
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            //Droga w dół
+            Console.Write(" ");
+            //Granica
+            Console.WriteLine("|");
+            //Środek przedział L5
+            Console.Write("---------");
+            //Środek droga L5
+            Console.Write(" ");
+            //Granica L5
+            Console.WriteLine("|");
+            //Miejsca parkingowe środek góra L6
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            //Droga w dół
+            Console.Write(" ");
+            //Granica
+            Console.WriteLine("|");
+            //Droga górna L7
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");//10 jest tego
+            //Granica
+            Console.WriteLine("|");
+            //Miejsca parkingowe górne L9
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            Console.Write("|");
+            Console.Write(" ");
+            //Granica drogi
+            Console.WriteLine("|");
+            //Górna granica L9
+            Console.WriteLine("------------");
+        }
+    }
+}
